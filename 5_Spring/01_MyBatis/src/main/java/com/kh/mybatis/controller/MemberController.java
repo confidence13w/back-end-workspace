a91c7781660a78fe9dae27732eb2bd276fcb0300
Grainer111 <confidence13w@gmail.com>
2024-07-31 14:40:48 +0900

    0731

--- 5_Spring/01_MyBatis/src/main/java/com/kh/mybatis/controller/MemberController.java
+++ 5_Spring/01_MyBatis/src/main/java/com/kh/mybatis/controller/MemberController.java
@@ -1,11 +1,15 @@
 package com.kh.mybatis.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.kh.mybatis.model.vo.Member;
 import com.kh.mybatis.service.MemberService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MemberController {
@@ -17,7 +21,8 @@ public class MemberController {
 	}
 	
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("allMember", service.allMember());
 		return "index";
 	}
 	
@@ -31,4 +36,39 @@ public class MemberController {
 		service.register(vo);
 		return "redirect:/";
 	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "mypage/login";
+	}
+	
+	@PostMapping("/login")
+	public String login(Member vo, HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		session.setAttribute("vo", service.login(vo));
+		return "redirect:/";
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		Member member = (Member) session.getAttribute("vo");
+		if(member!=null) session.invalidate();
+		return "redirect:/";
+	}
+	
+	@PostMapping("/update")
+	public String update(Member vo, HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		Member member = (Member) session.getAttribute("vo");
+		
+		if(vo.getId()==null) vo.setId(member.getId());
+		service.update(vo);
+		
+		if(vo.getName()==null) vo.setName(member.getName());		
+		session.setAttribute("vo", vo);
+
+		return "redirect:/";
+	}
+
 }
--- 5_Spring/01_MyBatis/src/main/java/com/kh/mybatis/service/MemberService.java
+++ 5_Spring/01_MyBatis/src/main/java/com/kh/mybatis/service/MemberService.java
@@ -1,5 +1,8 @@
 package com.kh.mybatis.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kh.mybatis.model.vo.Member;
@@ -9,14 +12,23 @@ import mapper.MemberMapper;
 @Service
 public class MemberService {
 
-	private MemberMapper memberMapper;
-	
-	public MemberService(MemberMapper memberMapper) {
-		this.memberMapper = memberMapper;
-	}
+	@Autowired
+	private MemberMapper mapper;
 	
 	public void register(Member member) {
-		memberMapper.register(member);
+		mapper.register(member);
+	}
+	
+	public List<Member> allMember() {
+		return mapper.allMember();
+	}
+	
+	public Member login(Member member) {
+		return mapper.login(member);
+	}
+
+	public void update(Member member) {
+		mapper.update(member);
 	}
 	
 }
